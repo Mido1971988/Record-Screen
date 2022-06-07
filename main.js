9509829940d1eb80b9600b9417a80aa7cd6e31ac
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2022-06-07 20:33:59 +0300

    first

--- main.js
+++ main.js
@@ -9,8 +9,7 @@ async function recordScreen() {
     const voiceStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false }); 
     let tracks = [...displayStream.getTracks(), ...voiceStream.getAudioTracks()] 
     const stream = new MediaStream(tracks); 
-    handleRecord({stream, mimeType}) 
-    window.alert("recordScreen Started")
+    handleRecord({stream, mimeType})
 }
 recordScreen()
 
@@ -19,19 +18,16 @@ const handleRecord = function ({stream, mimeType}) {
     start.addEventListener('click', (ev)=>{
         if(mediaRecorder.state === "inactive"){
         mediaRecorder.start();
-        window.alert("start Event")
         }
     })
     stop.addEventListener('click', (ev)=>{
         if(mediaRecorder.state === "recording" ){
             mediaRecorder.stop();
-            window.alert("Stop Event")
         }
     });    
     mediaRecorder.ondataavailable = function (e) {     
         if (e.data.size > 0) {       
             singleBlob = e.data
-            window.alert("onAvailabe")
         }        
     };   
     mediaRecorder.onstop = function () {      
@@ -40,6 +36,5 @@ const handleRecord = function ({stream, mimeType}) {
         videoEl.setAttribute("controls","")
         videoEl.src = videoURL;
         output.appendChild(videoEl)
-        window.alert("onStop")
     };  
 };
